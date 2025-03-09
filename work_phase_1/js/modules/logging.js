46e56eeb5b133ed0cc68b72adbe42da9f8bf6099
Praejtp <s4898675@student.uq.edu.au>
2025-03-09 15:03:55 +1000

    stating using JavaScript
    Linked the JS files to specific page which is js/index.js to index.html and created the module by importing and testing logMessage function from logging.js.  Testing by console.log from webpage

---
+++ work_phase_1/js/modules/logging.js
@@ -0,0 +1,10 @@
+/**
+ * Module: logging
+ * Generic logging using console.log
+ */
+
+function logMessage(pageName, message) {
+    console.log(`From {pageName}: {message}`);
+}
+
+export { logMessage };
