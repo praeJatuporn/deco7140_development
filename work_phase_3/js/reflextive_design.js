d21ce0b6f6eca509427c9a99263ec2d34d197a81
Praejtp <s4898675@student.uq.edu.au>
2025-05-04 17:15:25 +1000

    value mapping, tone & language expressed
    created the content of value mapping, imagenary and tone, language expression on reflective html page with accordion.

---
+++ work_phase_3/js/reflextive_design.js
@@ -0,0 +1,29 @@
+/**
+ * IMPORTS
+ * Keep track of external modules being used
+ * */
+import { initAccordion } from "./modules/accordion.js";
+/**
+ * CONSTANTS
+ * Define values that don't change e.g. page titles, URLs, etc.
+ * */
+const PAGE_NAME = "reflective_design.js";
+/**
+ * VARIABLES
+ * Define values that will change e.g. user inputs, counters, etc.
+ * */
+let message = "Page has fully loaded";
+/**
+ * FUNCTIONS
+ * Group code into functions to make it reusable
+ * */
+
+/**
+ * EVENT LISTENERS
+ * The code that runs when a user interacts with the page
+ * */
+
+// when the page fully loads
+document.addEventListener("DOMContentLoaded", () => {
+    initAccordion(".accordion");
+}); 
